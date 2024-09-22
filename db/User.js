becd1406872b5483002322e5b177f621d61baf21
Jurako <jurako91@gmail.com>
2024-09-22 21:55:07 +0300

    add password encryption

--- db/User.js
+++ db/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const validator = require('validator');
 const { isEmail } = validator;
 
@@ -26,5 +27,12 @@ const userSchema = new mongoose.Schema({
     phone: String,
     address: String 
 });
+
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+})
 
 module.exports = mongoose.model('User', userSchema);
