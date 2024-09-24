84071c5c9352074b75984d380bf0c5d4f4fc34fb
Jurako <jurako91@gmail.com>
2024-09-24 08:35:45 +0300

    add graceful restart

--- api.js
+++ api.js
@@ -54,3 +54,28 @@ function errorHandler(err) {
 
     return errors;
 }
+
+
+
+
+
+
+process.on('SIGTERM', stopHandler);
+process.on('SIGINT', stopHandler);
+process.on('SIGHUP', stopHandler);
+async function stopHandler() {
+  console.log('Stopping...');
+
+  const timeoutId = setTimeout(() => {
+    process.exit(1);
+    console.error('Stopped forcefully, not all connection was closed');
+  }, 2000);
+
+  try {
+    await server.stop();
+    clearTimeout(timeoutId);
+  } catch (error) {
+    console.error(error, 'Error during stop.');
+    process.exit(1);
+  }
+}
